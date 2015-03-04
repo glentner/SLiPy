04314cfef71ac80533b79dc761f192066ed19597
gLENTNER <geoffrey.lentner@louisville.edu>
2015-03-04 16:00:26 -0500

    updates

--- __init__.py
+++ __init__.py
@@ -2,16 +2,12 @@
 # See LICENSE (GPLv2)
 # Python/__init__.py
 """
- - Computational Astronomy Package for Python -
+AstroPython
+===========
+A Computational Astronomy Package for Python
 
 This Python package contains my code for performing computational work in 
-astronomy research. I've originally named this package simply `Python` because
-there seems to be an over abundance of acronyms in the current state of 
-astronomy computing with Python that I didn't want to pollute it any further.
-As such, I've implemented this project with `relative` imports within the 
-package source such that you can rename it whatever you please (i.e.,
-git clone http://github.com/glentner/Python; scp -r Python Susy)
-
+astronomy research.
 """
 
 # exposed modules
