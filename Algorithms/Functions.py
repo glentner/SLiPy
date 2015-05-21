749843e2c7003f8aeba80b7458b82f32ed4f070d
gLENTNER <geoffrey.lentner@louisville.edu>
2015-05-20 20:39:36 -0400

    NormalizedGaussian

--- Algorithms/Functions.py
+++ Algorithms/Functions.py
@@ -16,27 +16,34 @@ def Gaussian(x, *params):
     """
     A, mu, sigma = params
     return A * np.exp( -0.5 * (x - mu)**2 / sigma**2 )
-    
+
+def NormalizedGaussian(x *params):
+    """
+    The normalized Gaussian function.
+    """
+    mu, sigma = params
+    return np.exp( -0.5 * (x - mu)**2 / sigma**2 ) / (sigma *
+        np.sqrt(2 * np.pi))
+
 def InvertedGaussian(x, *params):
     """
-    An inverted Gaussian function (i.e., 1 - Guassian()). The function 
-    template has `*params` to work with scipy...curve_fit; the user *must* 
+    An inverted Gaussian function (i.e., 1 - Guassian()). The function
+    template has `*params` to work with scipy...curve_fit; the user *must*
     specify `A, mu, sigma = params`.
     """
     return 1 - Gaussian(x, *params)
-    
+
 def Lorentzian(x, *params):
     """
     The Lorentzian function.
     """
     x0, gamma = params
     return (0.5 * gamma / np.pi) / ( (x - x0)**2 + (0.5 * gamma)**2 )
-    
+
 def InvertedLorentzian(x, *params):
     """
     An inverted Lorentzian (i.e, A - Lorentzian()).
     """
     A, x0, gamma = params
-    
-    return A - Lorentzian(x, *params[1:])
-    
+
+    return A - Lorentzian(x, x0, gamma)
