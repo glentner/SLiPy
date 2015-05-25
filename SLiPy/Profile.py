8b636cd36847c7cd236244b810a3a9a4598a6e80
gLENTNER <geoffrey.lentner@louisville.edu>
2015-05-25 12:08:51 -0400

    Profile.MultiFit()

--- SLiPy/Profile.py
+++ SLiPy/Profile.py
@@ -22,7 +22,7 @@ from .Spectrum import Spectrum, SpectrumError
 from .Plot import SPlot, PlotError
 from ..Framework.Options import Options, OptionsError
 
-from ..Algorithms.Functions import Gaussian, InvertedLorentzian, NormalizedGaussian
+from ..Algorithms.Functions import Gaussian, Lorentzian, Voigt, InvertedLorentzian
 from ..Algorithms.KernelFit import KernelFit1D
 
 class ProfileError(SlipyError):
@@ -85,7 +85,7 @@ def Select(splot):
     input(' Press <Return> after making your selections ... ')
     return selected
 
-def Fit(splot, function = InvertedLorentzian, params = None):
+def AutoFit(splot, function = InvertedLorentzian, params = None):
     """
     Given `splot` of type SPlot, the user selects two points on the
     spectrum and a parameterized function is fit (an inverted Lorentzian by
@@ -270,20 +270,13 @@ class FittingGUI:
 	Graphical Interface (keeps references alive) for fitting analytical
 	profiles to spectral data.
 	"""
-	def __init__(self, fig, function='Lorentzian', observatory=None,
-		resolution=None, **kwargs):
+	def __init__(self, fig, function='Voigt', **kwargs):
 		"""
 		Build widget elements based on a spectrum plotted in `fig` (type SPlot).
 		`fig` may also be a Spectrum object (for which a SPlot will be
 		created).
 
-		Make initial guesses at parameters, render to figure. `LSP` is the
-		Line Spread Function from the instrument profile. This is convolved
-		with the profile `function`s to be fit. Usually, Lorentzian`s are fit
-		to the `line`s. The LSP is not a parameter to be adjusted and is
-		instead inforced for a given `Observatory` or `Resolution`. If not
-		given `Resolution` explicitely, one is expected to be provided by the
-		`Observatory`; one of these must be given.
+		Make initial guesses at parameters, build widgets, render the figure.
 		"""
 		try:
 
@@ -300,25 +293,9 @@ class FittingGUI:
 			print(' --> OptionsError:', err)
 			raise ProfileError('Unrecognized option given to Extract()!')
 
-		if function not in ['Lorentzian', 'Gaussian']:
+		if function not in ['Voigt', 'Lorentzian', 'Gaussian']:
 			raise ProfileError('The only currently implemented functions '
-			'for fitting profiles are `Lorentzian` and `Gaussian`!')
-
-		if observatory and not issubclass( type(observatory), Observatory):
-			raise ProfileError('FittingGUI() expects `observatory` to '
-			'be derived from the Observatory class!')
-
-		if not observatory and not resolution:
-			raise ProfileError('FittingGUI() expects either `resolution` or '
-			'an `observatory` (with `resolution` as an attribute) to be '
-			'provided as a keyword argument!')
-
-		if resolution and observatory and observatory.Resolution != resolution:
-			raise ProfileError('From FittingGUI(), you provided both an '
-			'`observatory` and a `resolution` but they don`t match!')
-
-		# resolution for the instrument
-		self.res = resolution if resolution else observatory.resolution
+			'for fitting profiles are `Voigt`, `Lorentzian` and `Gaussian`!')
 
 		# grab and/or create the SPlot
 		if isinstance(fig, Spectrum):
@@ -333,7 +310,7 @@ class FittingGUI:
 
 		# extract the line, continuum, rms from the spectrum
 		self.line, self.continuum, self.rms = Extract(fig, bandwidth=bandwidth,
-			rms=True)
+			kind=kind, rms=True)
 
 		print('\n Now select the peaks of each line to be fit.')
 		print(' Initial guesses will be made for each line markered.')
@@ -372,7 +349,7 @@ class FittingGUI:
 		# final spectral line profile is convolution of the LSP and the
 		# line profile function. Gaussian on Gaussian, Gaussian on Lorentzian,
 		# etc ... Set the functional form given requested profile function
-		self.Convolution = self.SetConvolution(function)
+		self.Evaluate = self.SetFunction(function)
 
 		# grab the actual Figure object and it's axis to keep references
 		self.fig = fig.fig
@@ -384,7 +361,7 @@ class FittingGUI:
 		fig.draw()
 
 		# bring up plot to make room for sliders
-		plt.subplots_adjust(bottom = 0.35)
+		plt.subplots_adjust(bottom = 0.30)
 
 		# resample continuum onto line domain
 		self.continuum = self.continuum.copy()
@@ -394,34 +371,41 @@ class FittingGUI:
 		self.x         = self.continuum.wave.value
 		self.continuum = self.continuum.data.value
 
+		# copy of continuum allows for adjustments
+		self.y = self.continuum.copy()
+
+		# save the mid-level of the continuum to avoid over-calculation
+		self.continuum_level = self.continuum.mean()
+
 		# add plots of each line, keep dictionary of handles
 		self.Component = {
 
 			line : plt.plot(self.x,
-				self.continuum - self.Convolution(self.x, **parameters),
-				'k--')[0]
+				self.y - self.Evaluate(self.x, **parameters), 'k--')[0]
 
 			for line, parameters in self.Params.items()
 		}
 
 		# add plot of superposition of each component
-		self.Combination, = plt.plot(self.x, self.continuum -
-			self.SuperPosition(), 'g-')
+		self.Combination, = plt.plot(self.x, self.y - self.SuperPosition(), 'g-')
 
 		# fix the limits on plot
 		xmin, xmax = fig.ax.get_xlim()
 		ymin, ymax = fig.ax.get_ylim()
 		plt.axis([xmin, xmax, ymin, ymax])
 
+		# axes for parameter sliders
 		self.Axis = {
 
 			# key : axis     xpos , ypos + dy       , xsize, ysize
-			line  : plt.axes([0.15, 0.05 + k * 0.045, 0.5, 0.035],
-				axisbg = 'white')
-
+			line  : plt.axes([0.13, 0.05 + (k+1) * 0.03, 0.65, 0.02], axisbg = 'white')
 			for k, line in enumerate( self.Params['L1'].keys() )
 		}
 
+		# add an axis to make adjustments to the continuum
+		self.Axis['Continuum'] = plt.axes([0.13, 0.05, 0.65, 0.02], axisbg='white')
+
+		# create the sliders for the parameters
 		self.Slider = {
 
 			# Slider `key` and widget
@@ -438,12 +422,17 @@ class FittingGUI:
 			for param in self.Params['L1'].keys()
 		}
 
+		# create the slider for the continuum (10% adjustments)
+		self.Slider['Continuum'] = widgets.Slider(self.Axis['Continuum'], 'Continuum',
+			0.90 * self.continuum_level, 1.10 * self.continuum_level,
+			valinit = self.continuum_level)
+
 		# connect sliders to update function
 		for slider in self.Slider.values():
 			slider.on_changed(self.Update)
 
 		# create axis for radio buttons
-		self.RadioAxis = plt.axes([0.75, 0.03, 0.1, 0.2],
+		self.RadioAxis = plt.axes([0.85, 0.01, 0.1, 0.2],
 			axisbg = 'white', frameon = False)
 
 		# create the radio button widget
@@ -456,10 +445,36 @@ class FittingGUI:
 		# set current component as the first line
 		self.current_component = 'L1'
 
+		# make currently selected line bold
+		self.Component['L1'].set_linewidth(2)
+
+		# variable allows for the radio buttons to not screw-up the sliders/graphs
+		# when switching between lines
+		self.stall = False
+
 	def Parameterize(self, function, loc):
 		"""
 		Choose the initial parameters of `function` given the peak `loc`.
 		"""
+
+		if function == 'Voigt':
+
+			return {
+
+					# from the Lorentzian
+					'Gamma': 0.50 * self.domainsize / self.numlines,
+
+					# from the Gaussian
+					'Sigma': 0.25 * self.domainsize / self.numlines,
+
+					# center of the line
+					'Peak' : loc[0],
+
+					# depth of the line
+					'Depth': self.continuum[ loc[0] ].value - loc[1]
+
+				}
+
 		if function == 'Lorentzian':
 
 			return {
@@ -468,7 +483,10 @@ class FittingGUI:
 					'FWHM': 0.5 * self.domainsize / self.numlines,
 
 					# center of the line
-					'Peak': loc[0]
+					'Peak': loc[0],
+
+					# depth of the line
+					'Depth': self.continuum[ loc[0] ].value - loc[1]
 				}
 
 		elif function == 'Gaussian':
@@ -476,41 +494,42 @@ class FittingGUI:
 			return {
 
 					# FWHM / Ln(2) of Gaussian
-					'FWHM': self.domainsize / (0.5 * self.numlines / 4),
+					'FWHM': 0.5 * self.domainsize / self.numlines,
 
 					# center of line
 					'Peak': loc[0],
 
-					# depth of line of line
+					# depth of the line
 					'Depth': self.continuum[ loc[0] ].value - loc[1]
 				}
 
-		else:
-			raise ProfileError('From FittingGUI.Parameterize(), the only '
+		else: raise ProfileError('From FittingGUI.Parameterize(), the only '
 			'currently implemented functions are the `Lorentzian` and '
 			'the `Gaussian`!')
 
-	def SetConvolution(self, function):
+	def SetFunction(self, function):
 		"""
-		Return which convolution result to use given profile function.
+		Return which Evaluate result to use given profile function.
 		"""
-		if function == 'Lorentzian':
 
-			return self.Gaussian_Lorentzian
+		options = {
 
-		elif function == 'Gaussian':
+			# local versions of functions handle parameters
+			'Voigt'     : self.__Voigt,
+			'Lorentzian': self.__Lorentzian,
+			'Gaussian'  : self.__Gaussian
+		}
 
-			return self.Gaussian_Gaussian
-
-		else:
-			raise ProfileError('From FittingGUI.SetConvolution(), the only '
-			'currently implemented functions are the `Lorentzian` and '
+		if function not in options:
+			raise ProfileError('From FittingGUI.SetFunction(), the only '
+			'currently implemented functions are the `Voigt`, `Lorentzian`, and '
 			'the `Gaussian`!')
 
-	def Gaussian_Gaussian(self, x, **params):
+		return options[function]
+
+	def __Gaussian(self, x, **params):
 		"""
-		Convolution of the Gaussian LSP on a Gaussian line feature.
-		The convolution of two gaussians is also a gaussian.
+		A Gaussian profile. See ..Algorithms.Functions.Gaussian
 		"""
 		return Gaussian(
 
@@ -527,17 +546,41 @@ class FittingGUI:
 			)
 
 
-	def Gaussian_Lorentzian(self, x, **params):
+	def __Lorentzian(self, x, **params):
 		"""
-		Convolution of the Gaussian LSP on a Lorentzian line feature.
-		This is the Voigt profile, and is proportional to the real part
-		of the Faddeeva function (slipy.special.wofz)
+		A Lorentzian profile. See ..Algorithms.Functions.Lorentzian
 		"""
+		return params['Depth'] * Lorentzian(
 
-		sigma_root_2 = (params['Peak'] / self.res) / (2 * np.sqrt(np.log(2)))
-		z = (( x - params['Peak'] ) + 1j * params['FWHM'] ) / sigma_root_2
+				x,
 
-		return w(z).real / (sigma_root_2 * np.sqrt(np.pi))
+				# location of the peak
+				params['Peak'],
+
+				# gamma *is* the FWHM
+				params['FWHM']
+			)
+
+	def __Voigt(self, x, **params):
+		"""
+		The Voigt profile. See ..Algorithms.Functions.Voigt
+		"""
+		return Voigt(
+
+				x,
+
+				# depth of the line
+				params['Depth'],
+
+				# location of the peak
+				params['Peak'],
+
+				# sigma from the Gaussian
+				params['Sigma'],
+
+				# gamma from the Lorentzian
+				params['Gamma']
+			)
 
 	def SuperPosition(self):
 		"""
@@ -548,7 +591,7 @@ class FittingGUI:
 
 		# additional lines
 		for parameters in self.Params.values():
-			combined += self.Convolution(self.x, **parameters)
+			combined += self.Evaluate(self.x, **parameters)
 
 		return combined
 
@@ -562,14 +605,22 @@ class FittingGUI:
 
 		elif param == 'FWHM':
 
-			return 0
+			# don't divide by zero!!!!
+			return 1e-6
 
 		elif param == 'Depth':
 
-			return 0
+			return 1e-6
 
-		else:
-			raise ProfileError('From FittingGUI.Minimum(), `{}` is not '
+		elif param == 'Sigma':
+
+			return 1e-6
+
+		elif param == 'Gamma':
+
+			return 1e-6
+
+		else: raise ProfileError('From FittingGUI.Minimum(), `{}` is not '
 			'currently implemented as a parameter to set the minumum '
 			'for!'.format(param))
 
@@ -583,14 +634,21 @@ class FittingGUI:
 
 		elif param == 'FWHM':
 
-			return self.domainsize
+			return 0.9 * self.domainsize
 
 		elif param == 'Depth':
 
 			return 1.5 * self.continuum.max()
 
-		else:
-			raise ProfileError('From FittingGUI.Maximum(), `{}` is not '
+		elif param == 'Sigma':
+
+			return 0.9 * self.domainsize / self.numlines
+
+		elif param == 'Gamma':
+
+			return 0.9 * self.domainsize / self.numlines
+
+		else: raise ProfileError('From FittingGUI.Maximum(), `{}` is not '
 			'currently implemented as a parameter to set the maximum '
 			'for!'.format(param))
 
@@ -599,22 +657,29 @@ class FittingGUI:
 		Cycle thru Sliders and update Parameter dictionary. Re-draw graphs.
 		"""
 
-		# the currently selected line component
-		line = self.current_component
+		if not self.stall:
 
-		# update the appropriate parameters in the dictionary
-		for parameter, slider in self.Slider.items():
-			self.Params[line][parameter] = slider.val
+			# the currently selected line component
+			line = self.current_component
 
-		# update the appropriate graph data, based on new parameters
-		self.Component[line].set_ydata(self.continuum -
-			self.Convolution(self.x, **self.Params[line]))
+			# update the appropriate parameters in the dictionary
+			for parameter, slider in self.Slider.items():
 
-		# update the super-imposed graphs
-		self.Combination.set_ydata(self.continuum - self.SuperPosition())
+				if parameter == 'Continuum':
+					self.y = self.continuum + (slider.val - self.continuum_level)
 
-		# push updates to graph
-		self.fig.canvas.draw_idle()
+				else:
+					self.Params[line][parameter] = slider.val
+
+			# update the appropriate graph data, based on new parameters
+			for line, parameters in self.Params.items():
+				self.Component[line].set_ydata(self.y - self.Evaluate(self.x, **parameters))
+
+			# update the super-imposed graphs
+			self.Combination.set_ydata(self.y - self.SuperPosition())
+
+			# push updates to graph
+			self.fig.canvas.draw_idle()
 
 	def ToggleComponent(self, label):
 		"""
@@ -625,7 +690,10 @@ class FittingGUI:
 		# reassign the current component that is selected
 		self.current_component = label
 
-		# make current feature bold and the rest regular
+		# don't update the parameter dictionary or draw the graph!!!!!!
+		self.stall = True
+
+		# make current feature bold and the rest thin
 		for line in self.Component.keys():
 			if line == label:
 				self.Component[line].set_linewidth(2)
@@ -634,7 +702,122 @@ class FittingGUI:
 
 		# update the sliders to reflect the current component
 		for parameter, slider in self.Slider.items():
-			slider.set_val(self.Params[label][parameter])
+			if parameter != 'Continuum':
+				slider.set_val(self.Params[label][parameter])
+
+		# give control back to sliders
+		self.stall = False
 
 		# push updates to graph
 		self.fig.canvas.draw_idle()
+
+	def GetSpectra(self):
+		"""
+		Return the current graphs as Spectrum objects.
+		"""
+		return [
+
+				Spectrum(
+						# build a spectrum based on numpy arrays from plot
+						gui.Component[graph].get_ydata() * gui.line.data.unit,
+						gui.x * gui.line.wave.unit
+					)
+
+				# for all `L1`, `L2`, etc...
+				for graph in sorted( gui.Component.keys() )
+			]
+
+	def GetContinuum(self):
+		"""
+		Return the current graph of the continuum.
+		"""
+		return Spectrum(
+
+				# data from the graph, with applied units
+				self.y * self.line.data.unit,
+				self.x * self.line.wave.unit
+			)
+
+def MultiFit(splot, measurements=True, resolution=1e5, function='Voigt', **kwargs):
+	"""
+	The MultiFit routine takes a `splot` figure (type SPlot) and allows the
+	user to interactively fit line profiles. `splot` may optionally be of type
+	Spectrum, in which case a SPlot figure will be created for you. This function
+	creates a *FittingGUI* object (not documented here) which uses the *Profile.Extract()*
+	routine first (*kwargs* as passed to this function). As in the Extract routine, the user
+	will select four points that mark the boundaries of the line (blended or otherwise)
+	and some surrounding continuum to sample. The *KernelFit1D* routine is applied with the
+	user specified *bandwidth* to smooth the noise out of the continuum and interpolate, of
+	type *kind*, across the line gap. After this, the user is asked to further select points
+	marking the peaks of the line(s). The number of selection points indicates the number
+	of lines to be fit. If you wish to deblend two or more lines, select all suspected
+	locations. These are not only used to determine the number of lines to fit but to make
+	initial guesses at the parameters for each line and determine reasonable scales for the
+	sliders.
+
+	After these selections, a slider for each parameter appears along with radio buttons for
+	each line. The user can interactively adjust the parameter(s) for a given line by
+	selecting it (the radio buttons) and dragging a slider. Each line is represented by a
+	black, dashed line in the plot. The currently selected line is bold. The combined
+	(i.e., blended) lines are plotted with a solid green line.
+
+	Each slider is labeled on the left side with the name of the parameter it controls. At
+	the right of each slider is the current value of that parameter. The radio buttons are
+	labeled "L1", "L2", etc. for each line.
+
+	This routine returns a list of Spectrum objects. The length of the list is equal to the
+	number of lines that were fit plus one for the continuum. This routine functions as both
+	a single line fitting tool as well as a deblending tool. By default, the final
+	parameterizations are attached to each spectrum as a dictionary. Also by default, various
+	measurements from the fitted profiles are attached to each returned Spectrum object. These
+	include the equivelent width (attached as `.ew`) and column depth (attached as `N`). This
+	behavior can be suppressed by giving the keyword argument `measurements = False`. The
+	measurements are of type `Measurement` (..Framework.Measurement.Measurement). This is
+	so the uncertainty for each measurement can be attached to its result.
+
+	Example:
+	```
+	Lines = Profile.MultiFit(fig, bandwidth = u.Angstrom / 10)
+	Lines[1].N
+	`Equivelent Width`: (1.4567 ^+ 0.0123 _- 0.0213) x 10^-3 Angstrom
+
+	```
+	"""
+
+	# running the user interface
+	gui = FittingGUI(splot, function=function, **kwargs)
+
+	input('\n Press <Return> when you are finished fitting lines ...')
+
+	# attach the parameter dictionaries to each spectrum
+	lines = gui.GetSpectra()
+	for a, parameterization in enumerate( sorted(gui.Params.keys()) ):
+		lines[a].parameters = gui.Params[parameterization]
+
+	if measurements:
+
+		print('\n Computing the Equivalent Widths ... ', end = '')
+
+		continuum = lines[-1]
+
+		# the ratio of the line to the continuum, percent absorption (inverted I guess)
+		I0_I = [ continuum / spectrum for spectrum in lines[:-1] ]
+
+		# RMS error from the continuum fit (i.e., KernelFit1D in Profile.Extract())
+		rms = gui.rms
+
+		# the error in each line is from both the continuum noise and the counts
+		line_errors = [
+
+				# the error is added in quadrature,
+				# error in the continuum is rms
+				# error in the line is rms * sqrt(I / I0)
+				Spectrum( rms * np.sqrt(1 + 1 / absorption.data) * continuum.data.unit,
+					continuum.wave)
+			]
+
+		# the Equivelent Width
+		EWs = [
+				simps(spectrum.wave, continuum - 1 / absorption) * spectrum.wave.unit
+				for spectrum, absorption in zip(lines, I0_I)
+			]
