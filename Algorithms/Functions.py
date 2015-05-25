8c31ad0335958ad06b6f5346ed9d1e1ed20c0a80
gLENTNER <geoffrey.lentner@louisville.edu>
2015-05-25 12:07:52 -0400

    formats + more functions

--- Algorithms/Functions.py
+++ Algorithms/Functions.py
@@ -6,6 +6,7 @@ Analytical functions used in SLiPy.
 """
 
 import numpy as np
+from scipy.special import wofz as w # Faddeeva function
 
 # No FunctionsError implimented yet
 
@@ -35,10 +36,60 @@ def InvertedGaussian(x, *params):
 
 def Lorentzian(x, *params):
     """
-    The Lorentzian function.
+    The Lorentzian function. Typically used to describe an absorption line profile.
     """
     x0, gamma = params
-    return (0.5 * gamma / np.pi) / ( (x - x0)**2 + (0.5 * gamma)**2 )
+    return 1 / ((2 * (x - x0) / gamma)**2 + 1)
+
+def NormalizedLorentzian(x, *params):
+    """
+    The Lorentzian function, normalized.
+    """
+    x0, gamma = params
+    return 2 * Lorentzian(x, *params) / (np.pi * gamma)
+
+def NormalizedVoigt(x, *params):
+    """
+    The Voigt function. The result of the convolution of a Lorentzian with one or more
+    Gaussians. Often used to describe an intrinsically Lorentzian absorption profile blurred
+    by Gaussian broadening from thermal motions and turbulance and another Gaussian instrument
+    profile. The result is also the real part of the Faddevva function (the complex probability
+    function), implemented by Scipy as wofz in scipy.special
+
+    This returns a normalized profile. The name `NormalizedVoigt` is to keep the convention
+    for the rest of this module. The amplitude controlling Voigt profile `Voigt` evaluates
+    this function first for `x - x0 = 0` to empirically `un-normalize` it before scaling by the
+    requested amplitude
+
+    Reference:
+        Olivero, J.J; R.L. Longbothum. ``Empirical fits to the Voigt line width:
+        A brief review''. Journal of Quantitative Spectroscopy and Radiative Transfer. Vol 17.
+        Issue 2. pages 223-236. Feb 1977
+
+    Parameters: `x0, sigma, gamma = params`
+
+        x0    -> center of the profile
+        sigma -> the Gaussian width
+        gamma -> the Lorentzian width
+    """
+    x0, sigma, gamma = params
+    return w( ((x-x0) + 1j*gamma) / (sigma * np.sqrt(np.pi)) ).real / (
+        sigma * np.sqrt(2 * np.pi) )
+
+def Voigt(x, *params):
+    """
+    The Voigt line profile. See ..Algorithms.Function.NormalizedVoigt for more information.
+    This function returns an amplitude controlled Voigt profile.
+
+    Parameters: `A, x0, sigma, gamma = params`
+
+        A     -> amplitude of the profile
+        x0    -> center of the profile
+        sigma -> the Gaussian width
+        gamma -> the Lorentzian width
+    """
+    return params[0] * NormalizedVoigt(x, *params[1:]) / NormalizedVoigt(0, 0, *params[2:])
+
 
 def InvertedLorentzian(x, *params):
     """
